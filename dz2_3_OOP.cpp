b51d8d33e87e88468aa0bc3baf358f02b32f0f16
Rost89 <50184907+Rost89@users.noreply.github.com>
2019-06-10 15:10:20 +0300

    Add files via upload
    ДЗ 2 по ООП

---
+++ dz2_3_OOP.cpp
@@ -0,0 +1,70 @@
+/*Карпенко Р.
+Создать классы Apple (яблоко) и Banana (банан), которые наследуют класс Fruit (фрукт). 
+У Fruit есть две переменные-члена: name (имя) и color (цвет). Добавить новый класс GrannySmith, 
+который наследует класс Apple.*/
+
+#include <iostream>
+#include <locale>
+
+using namespace std;
+
+class Fruit {
+private:
+	string name;
+	string color;
+public:
+	Fruit(string n = "", string c = "")
+	:name(n), color(c)
+	{
+	}
+	string getName() {
+		return name;
+	}
+	string getColor() {
+		return color;
+	}
+};
+
+class Apple : public Fruit {
+public: 
+	Apple(string n = "apple", string c = "red")
+	:Fruit(n,c)
+	{
+
+	}
+
+};
+class Banana : public Fruit {
+public:
+	Banana(string n = "banana", string c = "yellow")
+	:Fruit(n, c)
+	{
+
+	}
+
+};
+class GrannySmith : public Apple {
+public:
+	GrannySmith(string n = "granny smith", string c = "green")
+		:Apple(n, c)
+	{
+
+	}
+};
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	char sym;
+	Apple a;
+	Banana b;
+	GrannySmith c;
+
+	cout << "My " << a.getName() << " is " << a.getColor() << ".\n";
+	cout << "My " << b.getName() << " is " << b.getColor() << ".\n";
+	cout << "My " << c.getName() << " is " << c.getColor() << ".\n";
+
+	cin >> sym;
+	return 0;
+}
+
